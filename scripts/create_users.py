f8db01d2b62eb0274f477eed80d665a526f37b78
Abderrachid BELLAALI <abderrachid.bellaali@gmail.com>
2022-12-13 01:44:40 +0100

    ✨ add(start draft scripts): add scripts to create, update and desactivate users in AD

---
+++ scripts/create_users.py
@@ -0,0 +1,25 @@
+# - Sur base d’un CSV dont vous définissez et documentez la structure, créer
+# automatiquement des comptes utilisateurs Active Directory
+
+import csv
+from active_directory import create_user
+
+# Ouvre le fichier CSV en mode lecture
+with open("users.csv", "r") as csv_file:
+    # Lit les données du fichier CSV en utilisant le module CSV
+    csv_reader = csv.reader(csv_file)
+    # Parcoure les lignes du fichier CSV
+    for row in csv_reader:
+        # Récupère les données du fichier CSV
+        username = row[0]
+        password = row[1]
+        first_name = row[2]
+        last_name = row[3]
+        email = row[4]
+        # Crée un compte utilisateur dans Active Directory en utilisant l'API Active Directory
+        create_user(username, password, first_name, last_name, email)
+
+
+# Dans cet exemple, le fichier CSV doit avoir la structure suivante :
+
+# username,password,first_name,last_name,email
